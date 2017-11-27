18d8521ea3c2bdcab52f7ae5e6c411d5b2b026b8
Joseph <504116408@qq.com>
2017-11-27 15:06:00 +0800

    Initial commit

---
+++ app/src/main/java/com/geobim/teamtask/activity/MainActivity.java
@@ -0,0 +1,24 @@
+package com.geobim.teamtask.activity;
+
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2017/11/27.
+ */
+
+public class MainActivity extends BaseActivity{
+    @Override
+    protected void initVariables() {
+
+    }
+
+    @Override
+    protected void initViews(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void loadData() {
+
+    }
+}
---
+++ app/src/main/java/com/geobim/teamtask/activity/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.geobim.teamtask.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.geobim.teamtask.R;
+
+public class SplashActivity extends BaseActivity {
+
+    @Override
+    protected void initVariables() {
+
+    }
+
+    @Override
+    protected void initViews(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_splash);
+
+    }
+
+    @Override
+    protected void loadData() {
+
+    }
+}
